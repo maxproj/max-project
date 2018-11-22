6345d9434ece0d15de2e1fcb56e8ba9caf26f18c
maxproj <44858392+maxproj@users.noreply.github.com>
2018-11-22 15:16:19 +0300

    Add files via upload

--- Infodate/forms.py
+++ Infodate/forms.py
@@ -1,9 +1,8 @@
-from django import forms
+from django.forms import ModelForm
 from .models import Infodate
 
-class InfodateForm(forms.ModelForm):
+class InfodateForm(ModelForm):
 
     class Meta:
         model = Infodate
-        fields = ('date_create', 'type_engine', 'type_kpp', 'probeg_tek', 'probeg_all', 'type_expl', 'date_oil_last', 'date_liq_last', 'date_brake_last', 'date_to_last', 'to_make', 'conder',)
-
+        fields = ('date_create', 'type_engine', 'type_kpp', 'probeg_tek', 'probeg_all', 'type_expl', 'date_oil_last', 'date_liq_last', 'date_brake_last', 'to_make', 'date_to_last', 'conder',)
