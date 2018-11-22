b05527510f91cce71a6d2d853eb9017b7ab9c133
maxproj <44858392+maxproj@users.noreply.github.com>
2018-11-22 15:15:37 +0300

    Add files via upload

--- Infodate/models.py
+++ Infodate/models.py
@@ -12,32 +12,33 @@ STATUS_CHOICES = (
 )
 
 STATUS_CHOICES_ENG = (
-	('K7J', '1,4л/75л.с.(K7J)'),
-	('K7M7101', '1,6л/82л.с.(K7M710)'),
-	('K7M710', '1,6л/86л.с.(K7M710)'),
-	('K7M800', '1,6л/102л.с.(K7M800)'),
-	('K7M801', '1,6л/113л.с.')
+	('1,4л/75л.с.(K7J)', '1,4л/75л.с.(K7J)'),
+	('1,6л/82л.с.(K7M710)', '1,6л/82л.с.(K7M710)'),
+	('1,6л/86л.с.(K7M710)', '1,6л/86л.с.(K7M710)'),
+	('1,6л/102л.с.(K7M800)', '1,6л/102л.с.(K7M800)'),
+	('1,6л/113л.с. (К7М800)', '1,6л/113л.с.(К7М800)')
 )
 
 STATUS_CHOICES_KPP = (
-	('akpp', 'АКПП'),
-	('mkpp', 'МКПП'),
+	('АКПП', 'АКПП'),
+	('МКПП', 'МКПП'),
 )
 class Infodate(models.Model):
 	author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-	probeg_tek = models.CharField(max_length=8)
-	date_create = models.DateField(auto_now=False)
-	probeg_all = models.CharField(max_length=8)
-	date_publish = models.DateTimeField(auto_now_add=True)
-	conder = models.CharField(max_length=3, choices = STATUS_CHOICES, default='да')
-	type_expl = models.CharField(max_length=5, choices = STATUS_CHOICES_TYPE, default='новый')
-	type_engine = models.CharField(max_length=20, choices = STATUS_CHOICES_ENG, default='K7J')
-	type_kpp = models.CharField(max_length=4, choices = STATUS_CHOICES_KPP, default='mkkp')
-	date_oil_last = models.DateField(auto_now=False)
-	date_liq_last = models.DateField(auto_now=False)
-	date_brake_last = models.DateField(auto_now=False)
-	date_to_last = models.DateField(auto_now=False)
-	to_make = models.CharField(max_length=3, choices = STATUS_CHOICES, default='да')	
+	probeg_tek = models.CharField(max_length=8, verbose_name="текущий пробег",)
+	date_create = models.DateField(auto_now=False, verbose_name="дата приобретения",)
+	probeg_all = models.CharField(max_length=8, verbose_name="общий пробег",)
+	date_publish = models.DateTimeField(auto_now_add=True, verbose_name="дата публикации",)
+	conder = models.CharField(max_length=3, choices = STATUS_CHOICES, default='да', verbose_name="наличие кондиционера",)
+	type_expl = models.CharField(max_length=5, choices = STATUS_CHOICES_TYPE, default='новый', verbose_name="тип эксплуатации",)
+	type_engine = models.CharField(max_length=20, choices = STATUS_CHOICES_ENG, default='1,4л/75л.с.(K7J)', verbose_name="тип двигателя",)
+	type_kpp = models.CharField(max_length=4, choices = STATUS_CHOICES_KPP, default='МКПП', verbose_name="тип КПП",)
+	date_oil_last = models.DateField(auto_now=False, verbose_name="дата замены масла(посл.)",)
+	date_liq_last = models.DateField(auto_now=False, verbose_name="дата замены ТЖ(посл.)",)
+	date_brake_last = models.DateField(auto_now=False, verbose_name="дата замены ОЖ(посл.)",)
+	date_to_last = models.DateField(auto_now=False, verbose_name="дата ТО(посл.)",)
+	to_make = models.CharField(max_length=3, choices = STATUS_CHOICES, default='да', verbose_name="прохождение ТО",)
+
 	
 def publish(self):
 	self.date_publish = timezone.now()
